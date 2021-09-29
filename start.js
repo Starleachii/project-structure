4a1dfba59c283685d776036e6fb7f0646ee2e389
Daniel de Souza Lima Junior <Dewaldman@outlook.com>
2021-09-29 11:05:40 -0300

    refactor: fs. promise

--- start.js
+++ start.js
@@ -4,41 +4,38 @@ const fs = require('fs').promises;
 
 async function makeFolders(folders) {
     for (const index in folders) {
-        await fs.mkdir(path.join(__dirname, folders[index]), { recursive: true }, (error) => {
-            if (error)
-                return console.error(error.message);
+        await fs.mkdir(path.join(__dirname, folders[index]), { recursive: true })
 
-            console.log(`Directory created ${folders[index]}`);
-        });
+            .then(() => console.log(`Directory created ${folders[index]}`))
+            
+            .catch((error) => console.error(error.message));
     }
 }
-
 
 async function makeFiles(files) {
     for (const index in files) {
-        await fs.writeFile(files[index], "", function (error) {
-            if (error)
-                return console.error(error.message);
+        await fs.writeFile(files[index], "",)
 
-            console.log(`File created ${files[index]}`);
-        });
+        .then(()=> console.log(`File created ${files[index]}`))
+            
+        .catch((error) => console.error(error.message));
     }
 }
 
 
-async function main(){
+async function main() {
     const folders = [
         //app
         'src/app/controllers/',
         'src/app/models/',
         'src/app/services/',
         'src/app/utils/',
-    
+
         //database
         'src/database/config/',
         'src/database/migrations/',
         'src/database/seeds/',
-    
+
         //server
         'src/server/routes/',
         'src/server/middlewares/'
